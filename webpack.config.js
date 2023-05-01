a66b1621a7c7da7239d1971065c5f69543520b3d
mhickner44 <rmicu1903@gmail.com>
2023-05-01 08:42:33 -0400

    gh pages

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
 const path = require('path');
 
 module.exports = {
-//  mode: "development",
+ mode: "development",
   entry: './src/index.js',
-  // devtool: 'inline-source-map',
-  // devServer: {
-  //   static: './dist',
-  // },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
